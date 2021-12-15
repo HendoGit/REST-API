b16894fed83de724531db3204e2b7be512556ae6
Alex Henderson <alexhenderson270@hotmail.com>
2021-12-15 16:19:48 +0000

    Add method to parse payload

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ Primary file for the API
 //Dependencies
 var http = require('http')
 var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder;
+// String decoder library contains number of useful things - we just want those within StringDecoder part of the object
+
 //The server should respond to all requests with a string
 
 //Start the server, and have it listen on port 3000
@@ -22,12 +25,35 @@ var server = http.createServer(function(req, res){
     // Get the query string as an object
     var queryStringObject = parsedUrl.query;
 
+    // Get the HTTP Method
     var method = req.method.toLowerCase();
-    // Send the response
-    res.end('Hello World\n')
 
-    // Log the request path
-    console.log('Request recieved on path:'+trimmedPath+' with this method: ' + method + 'with these query string paramaters', queryStringObject)
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+
+    // Payloads come to the http server as a stream, so we need to collect the stream as it comes in, and coalesce that into one thing
+    // once that stream comes to an end
+    var buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+    req.on('end', function(){
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World\n');
+
+        // Log the request path
+        console.log('Request recieved with this payload: ',buffer);
+
+    })
+
+
+
+    
 });
 
 
